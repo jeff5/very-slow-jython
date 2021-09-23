2c68e9ac8688f1408d7c392b62574cf955eb291b
Jeff Allen <ja.py@farowl.co.uk>
2021-09-23 16:09:36 +0100

    Restructure the sequence API test with parameters
    This makes better use of JUnit: more readable in the test report and
    less complex structurally.

--- rt3/src/test/java/uk/co/farowl/vsj3/evo1/AbstractSequenceAPITest.java
+++ rt3/src/test/java/uk/co/farowl/vsj3/evo1/AbstractSequenceAPITest.java
@@ -2,14 +2,19 @@ package uk.co.farowl.vsj3.evo1;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 /**
  * Test the {@link PySequence} API class on a variety of types. We are
@@ -20,192 +25,169 @@ import org.junit.jupiter.api.Test;
  * To reach our main goal, we need only try enough types to exercise
  * every abstract method once in some type.
  */
-@DisplayName("The API class Abstract")
+@DisplayName("In the Abstract API for sequences")
 class AbstractSequenceAPITest extends UnitTestSupport {
 
     /**
-     * This abstract base forms a check-list of methods we mean to test.
+     * Provide a stream of examples as parameter sets to the tests of
+     * methods that do not mutate their arguments. Each argument object
+     * provides a reference value and a test object compatible with the
+     * parameterised test methods.
+     *
+     * @return the examples for non-mutating tests.
      */
-    abstract static class Standard {
-
-        /** Test {@link Abstract#size(Object) Abstract.size} */
-        abstract void supports_size() throws Throwable;
-
-/// **
-// * Test {@link Abstract#concat(Object, Object) Abstract.concat}
-// */
-// abstract void supports_concat() throws Throwable;
-//
-/// ** Test {@link Abstract#repeat(Object, int) Abstract.repeat} */
-// abstract void supports_repeat() throws Throwable;
-
-        /**
-         * Test {@link Abstract#getItem(Object, int) Abstract.getItem}
-         */
-        abstract void supports_getItem() throws Throwable;
-
-/// **
-// * Test {@link Abstract#setItem(Object, int, Object)
-// * Abstract.setItem}
-// */
-// abstract void supports_setItem() throws Throwable;
-//
-/// **
-// * Test {@link Abstract#delItem(Object, int) Abstract.delItem}
-// */
-// abstract void supports_delItem() throws Throwable;
-//
-/// **
-// * Test {@link Abstract#getSlice(Object, int, int)
-// * Abstract.getSlice}
-// */
-// abstract void supports_getSlice() throws Throwable;
-//
-/// **
-// * Test {@link Abstract#setSlice(Object, int, int, Object)
-// * Abstract.setSlice}
-// */
-// abstract void supports_setSlice() throws Throwable;
-//
-/// **
-// * Test {@link Abstract#delSlice(Object, int, int)
-// * Abstract.delSlice}
-// */
-// abstract void supports_delSlice() throws Throwable;
-//
-/// ** Test {@link Abstract#tuple(Object) Abstract.tuple} */
-// abstract void supports_tuple() throws Throwable;
-//
-/// ** Test {@link Abstract#list(Object) Abstract.list} */
-// abstract void supports_list() throws Throwable;
-//
-/// **
-// * Test {@link Abstract#count(Object, Object) Abstract.count}
-// */
-// abstract void supports_count() throws Throwable;
-//
-/// **
-// * Test {@link Abstract#contains(Object, Object)
-// * Abstract.contains}
-// */
-// abstract void supports_contains() throws Throwable;
-//
-/// ** Test {@link Abstract#in(Object, Object) Abstract.in} */
-// abstract void supports_in() throws Throwable;
-//
-//// Not to be confused with PyNumber.index
-/// **
-// * Test {@link Abstract#index(Object, Object) Abstract.index}
-// */
-// abstract void supports_index() throws Throwable;
+    static Stream<Arguments> readableProvider() {
+        return Stream.of(//
+                bytesExample(), //
+                bytesExample("a"), //
+                bytesExample("café crème"), //
+                tupleExample(), //
+                tupleExample(42), //
+                tupleExample(Py.None, 1, PyLong.TYPE) //
+        );
     }
 
-    /** There is just one implementation of {@code byte}. */
-    static abstract class SequenceTest extends Standard {
-
-        // Working variables for the tests
-        final ArrayList<Object> ref;
-        final Object obj;
-
-        SequenceTest(List<?> ref, Object obj) {
-            this.ref = new ArrayList<>(ref);
-            this.obj = obj;
-        }
-
-        @Override
-        @Test
-        void supports_size() throws Throwable {
-            Object r = Abstract.size(obj);
-            assertEquals(ref.size(), r);
-        }
-
-        @Override
-        @Test
-        void supports_getItem() throws Throwable {
-            final int N = ref.size();
-            for (int i = 0; i < N; i++) {
-                Object r = Abstract.getItem(obj, i);
-                assertEquals(ref.get(i), r);
-            }
-            // And again relative to the end -1...-N
-            for (int i = 1; i <= N; i++) {
-                Object r = Abstract.getItem(obj, -i);
-                assertEquals(ref.get(N - i), r);
-            }
-            Class<IndexError> ie = IndexError.class;
-            assertThrows(ie, () -> Abstract.getItem(obj, -(N + 1)));
-            assertThrows(ie, () -> Abstract.getItem(obj, N));
+    /**
+     * Construct an example with a Python {@code bytes}, from text.
+     *
+     * @param s to encode to bytes
+     * @return the example (a reference value and a test object)
+     */
+    static Arguments bytesExample(String s) {
+        try {
+            return bytesExample(s.getBytes("UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            fail("failed to encode bytes");
+            return arguments();
         }
     }
 
-    @Nested
-    @DisplayName("the bytes object")
-    static abstract class BytesTest extends SequenceTest {
-        BytesTest(byte[] ref) { super(asList(ref), new PyBytes(ref)); }
-
-        BytesTest(String s, String enc)
-                throws UnsupportedEncodingException {
-            this(s.getBytes(enc));
-        }
-
-        static ArrayList<Integer> asList(byte[] a) {
-            ArrayList<Integer> c = new ArrayList<>(a.length);
-            for (byte b : a) { c.add(b & 0xff); }
-            return c;
-        }
-
-        @Nested
-        @DisplayName("b''")
-        static class EmptyBytesTest extends BytesTest {
-            EmptyBytesTest() { super(new byte[0]); }
-        }
-
-        @Nested
-        @DisplayName("b'a'")
-        static class SingleBytesTest extends BytesTest {
-            SingleBytesTest() { super(new byte[] {97}); }
-        }
-
-        @Nested
-        @DisplayName("b'café crème' (utf-8)")
-        static class ShortBytesTest extends BytesTest {
-            ShortBytesTest() throws UnsupportedEncodingException {
-                super("café crème", "UTF-8");
-            }
-        }
+    /**
+     * Construct an example with a Python {@code bytes}, from bytes.
+     *
+     * @param a the bytes
+     * @return the example (a reference value and a test object)
+     */
+    static Arguments bytesExample(byte... a) {
+        PyBytes obj = new PyBytes(a);
+        ArrayList<Object> ref = new ArrayList<>(a.length);
+        for (byte b : a) { ref.add(b & 0xff); }
+        return arguments(ref, obj);
     }
 
-    @Nested
-    @DisplayName("the tuple object")
-    static abstract class TupleTest extends SequenceTest {
-        TupleTest(Object... ref) {
-            super(asList(ref), new PyTuple(ref));
-        }
-
-        static ArrayList<Object> asList(Object[] a) {
-            ArrayList<Object> c = new ArrayList<>(a.length);
-            for (Object o : a) { c.add(o); }
-            return c;
-        }
-
-        @Nested
-        @DisplayName("()")
-        static class EmptyTupleTest extends TupleTest {
-            EmptyTupleTest() { super(); }
-        }
-
-        @Nested
-        @DisplayName("('hello',)")
-        static class SingleTupleTest extends TupleTest {
-            SingleTupleTest() { super("hello"); }
-        }
-
-        @Nested
-        @DisplayName("(None, 1, int)")
-        static class ShortTupleTest extends TupleTest {
-            ShortTupleTest() throws UnsupportedEncodingException {
-                super(Py.None, 1, PyLong.TYPE);
-            }
-        }
+    /**
+     * Construct an example with a Python {@code tuple}, from arbitrary
+     * objects.
+     *
+     * @param a the objects
+     * @return the example (a reference value and a test object)
+     */
+    static Arguments tupleExample(Object... a) {
+        PyTuple obj = new PyTuple(a);
+        return arguments(List.of(a), obj);
     }
+
+    @DisplayName("Abstract.size")
+    @ParameterizedTest(name = "size({1})")
+    @MethodSource("readableProvider")
+    void supports_size(List<Object> ref, Object obj) throws Throwable {
+        Object r = Abstract.size(obj);
+        assertEquals(ref.size(), r);
+    }
+
+    /// **
+    // * Test {@link Abstract#concat(Object, Object) Abstract.concat}
+    // */
+    // void supports_concat(List<Object> ref, Object obj) throws
+    // Throwable;
+    //
+    /// ** Test {@link Abstract#repeat(Object, int) Abstract.repeat} */
+    // void supports_repeat(List<Object> ref, Object obj) throws
+    // Throwable;
+
+    @DisplayName("Abstract.getItem")
+    @ParameterizedTest(name = "getItem({1}, i)")
+    @MethodSource("readableProvider")
+    void supports_getItem(List<Object> ref, Object obj)
+            throws Throwable {
+        final int N = ref.size();
+        for (int i = 0; i < N; i++) {
+            Object r = Abstract.getItem(obj, i);
+            assertEquals(ref.get(i), r);
+        }
+        // And again relative to the end -1...-N
+        for (int i = 1; i <= N; i++) {
+            Object r = Abstract.getItem(obj, -i);
+            assertEquals(ref.get(N - i), r);
+        }
+        Class<IndexError> ie = IndexError.class;
+        assertThrows(ie, () -> Abstract.getItem(obj, -(N + 1)));
+        assertThrows(ie, () -> Abstract.getItem(obj, N));
+    }
+
+    /// **
+    // * Test {@link Abstract#setItem(Object, int, Object)
+    // * Abstract.setItem}
+    // */
+    // void supports_setItem(List<Object> ref, Object obj) throws
+    // Throwable;
+    //
+    /// **
+    // * Test {@link Abstract#delItem(Object, int) Abstract.delItem}
+    // */
+    // void supports_delItem(List<Object> ref, Object obj) throws
+    // Throwable;
+    //
+    /// **
+    // * Test {@link Abstract#getSlice(Object, int, int)
+    // * Abstract.getSlice}
+    // */
+    // void supports_getSlice(List<Object> ref, Object obj) throws
+    // Throwable;
+    //
+    /// **
+    // * Test {@link Abstract#setSlice(Object, int, int, Object)
+    // * Abstract.setSlice}
+    // */
+    // void supports_setSlice(List<Object> ref, Object obj) throws
+    // Throwable;
+    //
+    /// **
+    // * Test {@link Abstract#delSlice(Object, int, int)
+    // * Abstract.delSlice}
+    // */
+    // void supports_delSlice(List<Object> ref, Object obj) throws
+    // Throwable;
+    //
+    /// ** Test {@link Abstract#tuple(Object) Abstract.tuple} */
+    // void supports_tuple(List<Object> ref, Object obj) throws
+    // Throwable;
+    //
+    /// ** Test {@link Abstract#list(Object) Abstract.list} */
+    // void supports_list(List<Object> ref, Object obj) throws
+    // Throwable;
+    //
+    /// **
+    // * Test {@link Abstract#count(Object, Object) Abstract.count}
+    // */
+    // void supports_count(List<Object> ref, Object obj) throws
+    // Throwable;
+    //
+    /// **
+    // * Test {@link Abstract#contains(Object, Object)
+    // * Abstract.contains}
+    // */
+    // void supports_contains(List<Object> ref, Object obj) throws
+    // Throwable;
+    //
+    /// ** Test {@link Abstract#in(Object, Object) Abstract.in} */
+    // void supports_in(List<Object> ref, Object obj) throws Throwable;
+    //
+    //// Not to be confused with PyNumber.index
+    /// **
+    // * Test {@link Abstract#index(Object, Object) Abstract.index}
+    // */
+    // void supports_index(List<Object> ref, Object obj) throws
+    // Throwable;
+
 }
